ce3dc5f49f26e7b6a9d85b9863fd5f7cf93535ab
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-06-12 14:45:19 -0400

    changed css appearance a little and got timer wokring again

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-var secondsLeft = 75
+var secondsLeft = 75;
 var timeEl = document.querySelector("#timer");
 var startGame = document.querySelector("#startGame");
 var quizMain = document.querySelector("#quizMain");
-var secondDiv = document.querySelector("#secondDiv");
+// var questionPage = document.querySelector("#questionPage")
+// var arrayOfQuestions = document.querySelector("arrayOfQuestions");
 
+// var arrayOfQuestions = [
+//   {
+//     questionPrompt: "Commonly used data types DO NOT include:",
+//     selections: ["strings", "Booleans", "alerts", "numbers"],
+//     answer: "alerts",
+//   },
+
+//   {
+//     questionPrompt:
+//       "The condition in an if/else statement is enclosed within ______.",
+//     selections: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+//     answer: "parenthesis",
+//   },
+
+//   {
+//     questionPrompt: "Arrays in Javascript can be used to store _______.",
+//     selections: [
+//       "numbers & strings",
+//       "other arrays",
+//       "booleans",
+//       "all of the above",
+//     ],
+//     answer: "all of the above",
+//   },
+
+//   {
+//     questionPrompt:
+//       "String values must be enclosed within ______ when being assigned to variables.",
+//     selections: ["commas", "curly brackets", "quotes", "parenthesis"],
+//     answer: "quotes",
+//   },
+
+//   {
+//     questionPrompt:
+//       "A very usefool tool during development and debugging for printing content to the debugger is:",
+//     selections: ["Javascript", "terminal/bash", "for loops", "console.log"],
+//     answer: "console.log",
+//   },
+// ];
 function setTime() {
-    startGame.addEventListener("click", function setTime() {
+  startGame.addEventListener("click", function setTime() {
     quizMain.style.display = "none";
-    secondDiv.style.display = "block";
-    var timerInterval = setInterval(function() {
+    // questionPrompt.style.display = "block";
+    var timerInterval = setInterval(function () {
       secondsLeft--;
       timeEl.textContent = secondsLeft;
-  
-      if(secondsLeft === 0) {
+
+      if (secondsLeft === 0) {
         clearInterval(timerInterval);
       }
-
     }, 1000);
   });
 }
-  setTime();
+
+setTime();
+
